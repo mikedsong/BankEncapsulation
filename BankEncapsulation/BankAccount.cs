40fe31bca8a86fbb29578e87c97497fbf3386954
michaelsong95@yahoo.com <michaelsong95@yahoo.com>
2025-11-11 16:33:05 -0800

    Completed the exercise

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,18 @@
+﻿namespace BankEncapsulation;
+
+public class BankAccount
+{
+    private double _balance;
+
+    public void Deposit(double amount)
+    {
+        Console.WriteLine($"Simulating a deposit of {amount, 0:c} to you account!!");
+        _balance = amount;
+    }
+
+    public double GetBalance()
+    {
+        return _balance;
+    }
+
+}
